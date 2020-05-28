78a2edce9d989c5aeb98a2fdbff801aa051b9ef4
Marcel Ritter <64130092+rittermarcel@users.noreply.github.com>
2020-05-28 22:57:24 +0200

    Kleine verbesserung

--- Abgaben/Aufgabe5/scriptinhalt.ts
+++ Abgaben/Aufgabe5/scriptinhalt.ts
@@ -1,12 +1,14 @@
 namespace Aufgabe5 {
+    //Interface erstellen
     export interface Artikel {
+        bild: string;
         name: string;
         beschreibung: string;
         preis: string;
-        bild: string;
+        
     }
 
-    
+    //Kleidung
     const tshirtrot:    Artikel =  { name: "T-Shirt Rot",         beschreibung: "Hohes Material. Sehr beqeuem.",              preis: "15,00€", bild: "tshirtteuer.png" };
     const tshirtschwarz:    Artikel =  { name: "t-Shirtschwarz",   beschreibung: "Sehr bequem. Aus Baumwolle",                preis: "8,00€", bild: "tshirtbillig.png" };
     const hosegelb:   Artikel =  { name: "Hose gelb",  beschreibung: "Sitzt perfekt",                            preis: "14,00€", bild: "hose.png" };
@@ -16,7 +18,7 @@ namespace Aufgabe5 {
     const bälle:       Artikel =  { name: "Bälle",      beschreibung: "Nur vom besten",                                          preis: "6,50€", bild: "tennisbälle.png" };
 
     
-   
+   //Equipment
     const schläger:      Artikel =  { name: "Schläger",      beschreibung: "Ohne Schläger geht nix",                        preis: "78,00€",    bild: "schläger.png" };
     const schuhe:         Artikel =  { name: "Schuhe",         beschreibung: "Ultra Geschwindigkeit",                                              preis: "22,00€",     bild: "schuh.png" };
     const dämpfer:        Artikel =  { name: "Dämpfer",         beschreibung: "Dämpft den Schlag",                         preis: "1,99€",     bild: "dämpfer.png" };
@@ -24,6 +26,7 @@ namespace Aufgabe5 {
     const tasche:        Artikel =  { name: "Tasche",        beschreibung: "Sieht professionell aus",                                   preis: "45,00€",     bild: "tasche.png" };
 
 
+    //In array speichern
     export let artikelkleidung: Artikel[] = [tshirtrot, tshirtschwarz, hosegelb, kapperot, brille, socken, bälle];
     export let artikelequipment: Artikel[] = [schläger, schuhe, dämpfer, bandage, tasche];
     }
