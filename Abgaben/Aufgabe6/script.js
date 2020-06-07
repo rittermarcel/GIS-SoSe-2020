24bb08a62a181e68f62d1f188afd64d238039e8d
Marcel Ritter <64130092+rittermarcel@users.noreply.github.com>
2020-06-07 19:24:37 +0200

    Unnötige kommentare entfernt da issue

--- Abgaben/Aufgabe6/script.js
+++ Abgaben/Aufgabe6/script.js
@@ -1,48 +1,36 @@
 "use strict";
 var Aufgabe6;
 (function (Aufgabe6) {
-    //Aufgabe 6
-    //Klasse Inhalt
     let divinhalt = document.createElement("div");
     divinhalt.setAttribute("class", "inhalt");
-    // H2 Titel Kleidung mit ID
     let kleidungtitel = document.createElement("h2");
     kleidungtitel.setAttribute("id", "Kleidunglink");
     kleidungtitel.innerHTML = "Kleidung";
     divinhalt.appendChild(kleidungtitel);
-    //Klasse Kleidung
     let kleidungdiv = document.createElement("div");
     kleidungdiv.setAttribute("id", "Kleidung");
     divinhalt.appendChild(kleidungdiv);
     let preis = 0.00;
-    //Schleife die den array mit den Kleidern komplett durchgeht. 
     for (let i = 0; i < Aufgabe6.artikelkleidung.length; i++) {
-        //Jeder neue Artikel wird in Klasse artikel gespeichern
         let divElement = document.createElement("div");
         divElement.setAttribute("class", "artikel");
         kleidungdiv.appendChild(divElement);
-        //Bild hinzufügen
         let bildElement = document.createElement("img");
         bildElement.setAttribute("src", Aufgabe6.artikelkleidung[i].bild);
         divElement.appendChild(bildElement);
-        //Titel hinzufügen
         let titelElement = document.createElement("h3");
         divElement.appendChild(titelElement);
         titelElement.innerHTML = Aufgabe6.artikelkleidung[i].name;
-        //Beschreibung hinzufügen
         let beschreibungelement = document.createElement("p");
         divElement.appendChild(beschreibungelement);
         beschreibungelement.innerHTML = Aufgabe6.artikelkleidung[i].beschreibung;
-        //Preis hinzufügen
         let preiselement = document.createElement("i");
         divElement.appendChild(preiselement);
         preiselement.innerHTML = Aufgabe6.artikelkleidung[i].preis + "€";
-        //Br tags
         let brelement = document.createElement("br");
         divElement.appendChild(brelement);
         let brelement2 = document.createElement("br");
         divElement.appendChild(brelement2);
-        //Kaufen Button
         let buttonElement = document.createElement("button");
         buttonElement.innerHTML = "Kaufen";
         buttonElement.addEventListener("click", allgemeinbutton);
@@ -53,16 +41,13 @@ var Aufgabe6;
             console.log("Gesamtsumme: " + preis + " €");
         }
     }
-    //H2 Equipment Titel mit ID
     let equipmentTitel = document.createElement("h2");
     equipmentTitel.setAttribute("id", "Equipmentlink");
     equipmentTitel.innerHTML = "Equipment";
     divinhalt.appendChild(equipmentTitel);
-    //neue Klasse Equipment
     let equipmentdiv = document.createElement("div");
     equipmentdiv.setAttribute("id", "Equipment");
     divinhalt.appendChild(equipmentdiv);
-    //Das selbe Spiel wie vorhin
     for (let i = 0; i < Aufgabe6.artikelequipment.length; i++) {
         let divElement = document.createElement("div");
         divElement.setAttribute("class", "artikel");
@@ -99,7 +84,6 @@ var Aufgabe6;
         counterbeginn++;
         counter.innerHTML = "" + counterbeginn;
     }
-    //Den ganzen Inhalt oben in den html main Tag hinzufügen
     document.getElementById("main")?.appendChild(divinhalt);
 })(Aufgabe6 || (Aufgabe6 = {}));
 const kleidungTitel = document.getElementById("Kleidunglink");
--- Abgaben/Aufgabe6/script.ts
+++ Abgaben/Aufgabe6/script.ts
@@ -1,63 +1,63 @@
 namespace Aufgabe6 {
     
-//Aufgabe 6
 
 
 
 
 
-    //Klasse Inhalt
+
+    
     let divinhalt: HTMLElement = document.createElement("div");
     divinhalt.setAttribute("class", "inhalt");
     
-    // H2 Titel Kleidung mit ID
+  
     let kleidungtitel: HTMLElement = document.createElement("h2");
     kleidungtitel.setAttribute("id", "Kleidunglink");
     kleidungtitel.innerHTML = "Kleidung";
     divinhalt.appendChild(kleidungtitel);
 
-    //Klasse Kleidung
+    
     let kleidungdiv: HTMLElement = document.createElement("div");
     kleidungdiv.setAttribute("id", "Kleidung");
     divinhalt.appendChild(kleidungdiv);
 
     let preis: number = 0.00;
-    //Schleife die den array mit den Kleidern komplett durchgeht. 
+    
     for (let i: number = 0; i < artikelkleidung.length; i++) {
 
-        //Jeder neue Artikel wird in Klasse artikel gespeichern
+        
         let divElement: HTMLElement = document.createElement("div");
         divElement.setAttribute("class", "artikel");
         kleidungdiv.appendChild(divElement);
 
-        //Bild hinzufügen
+       
         let bildElement: HTMLElement = document.createElement("img");
         bildElement.setAttribute("src", artikelkleidung[i].bild);
         divElement.appendChild(bildElement);
 
-        //Titel hinzufügen
+        
         let titelElement: HTMLElement = document.createElement("h3");
         divElement.appendChild(titelElement);
         titelElement.innerHTML = artikelkleidung[i].name;
 
-        //Beschreibung hinzufügen
+        
         let beschreibungelement: HTMLElement = document.createElement("p");
         divElement.appendChild(beschreibungelement);
         beschreibungelement.innerHTML = artikelkleidung[i].beschreibung;
 
-        //Preis hinzufügen
+      
         let preiselement: HTMLElement = document.createElement("i");
         divElement.appendChild(preiselement);
         preiselement.innerHTML = artikelkleidung[i].preis + "€";
 
-        //Br tags
+       
         let brelement: HTMLElement = document.createElement("br");
         divElement.appendChild(brelement);
 
         let brelement2: HTMLElement = document.createElement("br");
         divElement.appendChild(brelement2);
 
-        //Kaufen Button
+        
         let buttonElement: HTMLElement = document.createElement("button");
         buttonElement.innerHTML = "Kaufen";
         buttonElement.addEventListener("click", allgemeinbutton);
@@ -70,20 +70,20 @@ namespace Aufgabe6 {
              }
     }
 
-    //H2 Equipment Titel mit ID
+    
     let equipmentTitel: HTMLElement = document.createElement("h2");
     equipmentTitel.setAttribute("id", "Equipmentlink");
     equipmentTitel.innerHTML = "Equipment";
     divinhalt.appendChild(equipmentTitel);
 
-    //neue Klasse Equipment
+  
     let equipmentdiv: HTMLElement = document.createElement("div");
     equipmentdiv.setAttribute("id", "Equipment");
     divinhalt.appendChild(equipmentdiv);
 
 
 
-    //Das selbe Spiel wie vorhin
+
     for (let i: number = 0; i < artikelequipment.length; i++) {
         
 
@@ -139,7 +139,7 @@ namespace Aufgabe6 {
        
        
 
-//Den ganzen Inhalt oben in den html main Tag hinzufügen
+
     document.getElementById("main")?.appendChild(divinhalt);
     
 }
