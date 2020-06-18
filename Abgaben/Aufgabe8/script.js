1c5e5f4aad907ab17585b71bacf0fafb795d952c
Marcel Ritter <64130092+rittermarcel@users.noreply.github.com>
2020-06-18 14:42:26 +0200

    Aufgabe8

--- Abgaben/Aufgabe8/script.js
+++ Abgaben/Aufgabe8/script.js
@@ -6,18 +6,17 @@ var Aufgabe8;
     abschicken.addEventListener("click", abschickenklick);
     async function addToURL() {
         formData = new FormData(document.forms[0]);
-        let url = "https://gissommersemester2020.herokuapp.com/";
         let query = new URLSearchParams(formData);
-        url = url + "?" + query.toString();
+        let url = "https://gissommersemester2020.herokuapp.com/" + "?" + query.toString();
         return url;
     }
     async function abschickenklick() {
         getResponse(await addToURL());
     }
     async function getResponse(_url) {
-        let response = await fetch(_url, { method: "get" });
-        let resp2 = await response.text();
-        console.log(resp2);
+        let antwort = await fetch(_url, { method: "get" });
+        let antwort2 = await antwort.text();
+        console.log(antwort2);
     }
 })(Aufgabe8 || (Aufgabe8 = {}));
 //# sourceMappingURL=script.js.map
--- Abgaben/Aufgabe8/script.ts
+++ Abgaben/Aufgabe8/script.ts
@@ -5,9 +5,8 @@ namespace Aufgabe8 {
 
     async function addToURL(): Promise<string> {
         formData = new FormData(document.forms[0]);
-        let url: string = "https://gissommersemester2020.herokuapp.com/";
         let query: URLSearchParams = new URLSearchParams(<any>formData);
-        url = url + "?" + query.toString();
+        let url: string = "https://gissommersemester2020.herokuapp.com/" + "?" + query.toString();
         return url;
     }
 
@@ -16,8 +15,8 @@ namespace Aufgabe8 {
     }
 
     async function getResponse(_url: RequestInfo): Promise<void> {
-        let response: Response = await fetch(_url, { method: "get" });
-        let resp2: string = await response.text();
-        console.log(resp2);
+        let antwort: Response = await fetch(_url, { method: "get" });
+        let antwort2: string = await antwort.text();
+        console.log(antwort2);
 }
 }
